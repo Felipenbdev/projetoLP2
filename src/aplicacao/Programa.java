4cb90b7b6860956d9458a2af3df46d143b4da262
Felipe Nunes <nunessfelipee45@gmail.com>
2024-04-14 10:46:48 -0300

    implementacao da classe UX

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,130 +1,22 @@
 package aplicacao;
-
 import entidades.Comprador;
-import entidades.GerenciadorProdutos;
-
 import java.util.Scanner;
+import static aplicacao.UX.telaDeCompra;
+import static aplicacao.UX.telaDeLogin;
 
 public class Programa {
+    public static Comprador comprador = new Comprador();
     public static void limparTela(){
         System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Comprador comprador = null;
-
         System.out.println("Bem-vindo a Loja taltaltal");
-
-        comprador = criarComprador(sc);
-
-        boolean loggedIn = false;
-
-        while (!loggedIn) {
-            menuLogin();
-            int ops = sc.nextInt();
-            sc.nextLine(); // Limpa o buffer de entrada
-
-            switch (ops) {
-                case 1:
-                    System.out.println("Você já está cadastrado. Por favor, faça login.");
-
-                    break;
-                case 2:
-                    if (comprador != null) {
-                        loggedIn = fazerLogin(sc, comprador);
-                    } else {
-                        System.out.println("Você ainda não está cadastrado. Por favor, cadastre-se primeiro.");
-                    }
-                    break;
-                default:
-                    System.out.println("Opção inválida.");
-                    break;
-            }
-        }
+        telaDeLogin();
         limparTela();
-        int categoria = 0;
-        // Loop para comprar produtos
-        boolean continuarComprando = true;
-        while (continuarComprando) {
-            do{
-            
-            System.out.println("-=-=-=-=-=-=- Menu de compras -=-=-=-=-=-=-");
-            System.out.print("[1] Bebidas\n[2] Livros\n[3] Eletrônicos\n[4] Roupas\n[5] Alimentos\n[0] Sair\n>>");
-            categoria = sc.nextInt();
-            GerenciadorProdutos gerenciador = new GerenciadorProdutos();
-            System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
-    
-            switch (categoria) {
-                case 0:
-                    continuarComprando = false;
-                    categoria = 0;
-                    break;
-                case 1:
-                    System.out.println("Bebidas:");
-                    for(int i = 0; i < 5; i++) {
-                        System.out.println(gerenciador.getProdutos(0, i));
-                    }
-                    break;
-                case 2:
-                    System.out.println("Livros:");
-                    for(int i = 0; i < 5; i++) {
-                        System.out.println(gerenciador.getProdutos(1, i));
-                    }
-                    break;
-                case 3:
-                    System.out.println("Eletrônicos:");
-                    for(int i = 0; i < 5; i++) {
-                        System.out.println(gerenciador.getProdutos(2, i));
-                    }
-                    break;
-                case 4:
-                    System.out.println("Roupas:");
-                    for(int i = 0; i < 5; i++) {
-                        System.out.println(gerenciador.getProdutos(3, i));
-                    }
-                    break;
-                case 5:
-                    System.out.println("Alimentos:");
-                    for(int i = 0; i < 5; i++) {
-                        System.out.println( gerenciador.getProdutos(4, i));
-                    }
-                    break;
-                default:
-                    System.out.println("Categoria inválida.");
-                    break;
-            }
-            }while(categoria != 0);
-            System.out.print("Digite o nome do produto que deseja comprar: ");
-            comprador.fazerCompra(sc.nextLine());
-            System.out.print("Deseja continuar comprando? [s/n] \n>> ");
-            continuarComprando = sc.next().toLowerCase().charAt(0) == 's';
-            sc.nextLine(); // Limpa o buffer de entrada
-        }
-        // Após o loop de compras, faz o logout
+        telaDeCompra();
         comprador.fazerLogout();
         System.out.println("Você saiu da sua conta.");
-    }
-
-    public static void menuLogin() {
-        System.out.println("1-Novo usuário");
-        System.out.println("2-Fazer login");
-    }
-
-    public static Comprador criarComprador(Scanner sc) {
-        System.out.print("Digite seu nome: ");
-        String nome = sc.nextLine();
-        System.out.print("Digite seu email: ");
-        String email = sc.nextLine();
-        System.out.print("Digite sua senha: ");
-        String senha = sc.nextLine();
-        return new Comprador(nome, email, senha);
-    }
-
-    public static boolean fazerLogin(Scanner sc, Comprador comprador) {
-        System.out.print("Digite seu email: ");
-        String emailLogin = sc.nextLine();
-        System.out.print("Digite sua senha: ");
-        String senhaLogin = sc.nextLine();
-        return comprador.autenticacao(emailLogin, senhaLogin);
     }
 }
---
+++ src/aplicacao/UX.java
@@ -0,0 +1,121 @@
+package aplicacao;
+
+import entidades.Comprador;
+import entidades.GerenciadorProdutos;
+
+import java.util.Scanner;
+
+import static aplicacao.Programa.comprador;
+
+public class UX {
+    static Scanner sc = new Scanner(System.in);
+    public static void criarComprador() {
+        System.out.print("Digite seu nome: ");
+        String nome = sc.nextLine();
+        System.out.print("Digite seu email: ");
+        String email = sc.nextLine();
+        System.out.print("Digite sua senha: ");
+        String senha = sc.nextLine();
+
+        comprador.setEmail(email);
+        comprador.setNome(nome);
+        comprador.setSenha(senha);
+    }
+
+    public static boolean fazerLogin(Scanner sc, Comprador comprador) {
+        System.out.print("Digite seu email: ");
+        String emailLogin = sc.nextLine();
+        System.out.print("Digite sua senha: ");
+        String senhaLogin = sc.nextLine();
+        return comprador.autenticacao(emailLogin, senhaLogin);
+    }
+    public static void menuLogin() {
+        System.out.println("1-Novo usuário");
+        System.out.println("2-Fazer login");
+    }
+    public static void telaDeLogin(){
+        boolean loggedIn = false;
+        while (!loggedIn) {
+            UX.menuLogin();
+            int ops = sc.nextInt();
+            sc.nextLine();
+
+            switch (ops) {
+                case 1:
+                    criarComprador();
+                    System.out.println("Você já está cadastrado. Por favor, faça login.");
+                    break;
+                case 2:
+                    if (comprador != null) {
+                        loggedIn = UX.fazerLogin(sc, comprador);
+                    } else {
+                        System.out.println("Você ainda não está cadastrado. Por favor, cadastre-se primeiro.");
+                    }
+                    break;
+                default:
+                    System.out.println("Opção inválida.");
+                    break;
+            }
+        }
+    }
+    public static void telaDeCompra(){
+        int categoria = 0;
+        // Loop para comprar produtos
+        boolean continuarComprando = true;
+        while (continuarComprando) {
+            do{
+
+                System.out.println("-=-=-=-=-=-=- Menu de compras -=-=-=-=-=-=-");
+                System.out.print("[1] Bebidas\n[2] Livros\n[3] Eletrônicos\n[4] Roupas\n[5] Alimentos\n[0] Sair\n>>");
+                categoria = sc.nextInt();
+                GerenciadorProdutos gerenciador = new GerenciadorProdutos();
+                System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+
+                switch (categoria) {
+                    case 0:
+                        continuarComprando = false;
+                        categoria = 0;
+                        break;
+                    case 1:
+                        System.out.println("Bebidas:");
+                        for(int i = 0; i < 5; i++) {
+                            System.out.println(gerenciador.getProdutos(0, i));
+                        }
+                        break;
+                    case 2:
+                        System.out.println("Livros:");
+                        for(int i = 0; i < 5; i++) {
+                            System.out.println(gerenciador.getProdutos(1, i));
+                        }
+                        break;
+                    case 3:
+                        System.out.println("Eletrônicos:");
+                        for(int i = 0; i < 5; i++) {
+                            System.out.println(gerenciador.getProdutos(2, i));
+                        }
+                        break;
+                    case 4:
+                        System.out.println("Roupas:");
+                        for(int i = 0; i < 5; i++) {
+                            System.out.println(gerenciador.getProdutos(3, i));
+                        }
+                        break;
+                    case 5:
+                        System.out.println("Alimentos:");
+                        for(int i = 0; i < 5; i++) {
+                            System.out.println( gerenciador.getProdutos(4, i));
+                        }
+                        break;
+                    default:
+                        System.out.println("Categoria inválida.");
+                        break;
+                }
+            }while(categoria != 0);
+            System.out.print("Digite o nome do produto que deseja comprar: ");
+            comprador.fazerCompra(sc.nextLine());
+            System.out.print("Deseja continuar comprando? [s/n] \n>> ");
+            continuarComprando = sc.next().toLowerCase().charAt(0) == 's';
+            sc.nextLine(); // Limpa o buffer de entrada
+        }
+    }
+}
